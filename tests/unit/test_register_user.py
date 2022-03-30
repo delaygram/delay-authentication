f891cea3bfb861665a2078ab3ba8ddd48ace30e3
Rick Meels <rick.meels@gmail.com>
2022-03-30 09:45:11 +0200

    Added unit tests for user registration #4

--- tests/unit/test_register_user.py
+++ tests/unit/test_register_user.py
@@ -6,6 +6,7 @@ from unittest import mock
 from moto import mock_ssm, mock_cognitoidp, mock_events
 from functions.register import app
 
+
 @mock.patch.dict(os.environ, {"StageName": "test", 'DISABLE_XRAY': 'true', "EVENT_BUS": "test_event_bus"})
 @mock_ssm
 @mock_cognitoidp
@@ -34,7 +35,7 @@ class TestRegisterUser(unittest.TestCase):
 
     def tearDown(self):
         self.cognito_mock.delete_user_pool(UserPoolId=self.user_pool_id)
-        self.ssm_mock.delete_parameter(Name=['DelaygramAuthorizerUserPoolId-test','DelaygramAuthorizerClientId-test'])
+        self.ssm_mock.delete_parameter(Name=['DelaygramAuthorizerUserPoolId-test', 'DelaygramAuthorizerClientId-test'])
 
     def test_register_user_happy_flow(self):
         ret = self.register_test_user()
@@ -45,3 +46,61 @@ class TestRegisterUser(unittest.TestCase):
         assert registered_users[0]['Username'] == 'test_user'
 
     def test_register_user_alternate_flow_username_already_taken(self):
+        self.register_test_user()
+        registration_request = {
+            'username': 'test_user',
+            'password': 'test_password',
+            'email': 'test2@email.com'
+        }
+
+        register_event = {
+            'body': json.dumps(registration_request)
+        }
+
+        context = 'not used'
+        ret = app.lambda_handler(register_event, context)
+
+        assert ret['statusCode'] == 409
+        assert ret['body'] == json.dumps({
+            'type': 'UsernameExistsException',
+            'message': 'test_user'
+        })
+
+    # def test_confirm_registration_happy_flow(self):
+    #     self.register_test_user()
+    #
+    #     confirmation_request = {
+    #         'username': 'test_user',
+    #         'code': '123456'
+    #     }
+    #
+    #     confirmation_event = {
+    #         'body': json.dumps(confirmation_request)
+    #     }
+    #
+    #     context = 'not used'
+    #     ret = app.lambda_handler(confirmation_event, context)
+    #
+    #     assert ret['statusCode'] == 200
+    #     assert ret['body'] == json.dumps({'username': 'test_user'})
+
+    @staticmethod
+    def register_test_user():
+        registration_request = {
+            'username': 'test_user',
+            'password': 'test_password',
+            'email': 'test@email.com'
+        }
+
+        register_event = {
+            'body': json.dumps(registration_request)
+        }
+
+        context = 'not used'
+        return app.lambda_handler(register_event, context)
+
+    def admin_confirm_test_user(self):
+        self.cognito_mock.admin_confirm_sign_up(
+            UserPoolId=self.user_pool_id,
+            Username='test_user'
+        )
