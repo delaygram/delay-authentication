cd9fc7ec50e29fb166b2ba8ebe3e86c9113ede2c
Rick Meels <rick.meels@gmail.com>
2022-04-06 14:33:26 +0200

    full implementation for registration, confirmation and logging in

--- functions/confirm/app.py
+++ functions/confirm/app.py
@@ -1,7 +1,6 @@
 import base64
 import hashlib
 import hmac
-import logging
 import os
 
 import boto3
@@ -13,41 +12,41 @@ def lambda_handler(event, context):
     cognito = boto3.client('cognito-idp')
     event_bus = boto3.client('events')
 
-    logging.info(f'Received event: {event}')
+    print(f'Received event: {event}')
 
     body = json.loads(event['body'])
     username = body['username']
     code = body['code']
 
     try:
-        logging.info(f'Confirming user {username}')
+        print(f'Confirming user {username}')
 
         response = cognito.confirm_sign_up(
             ClientId=get_client_id(),
             Username=username,
             SecretHash=get_secret_hash(username, get_client_id(), get_user_pool_id()),
-            ConfirmationCode=str(code)
+            ConfirmationCode=code
         )
 
-        logging.info(f'Confirmation response: {response}')
+        print(f'Confirmation response: {response}')
 
         user_id = retrieve_user_id(username)
 
-        logging.info(f'Publishing event for user {user_id} : {username}')
+        print(f'Publishing event for user {user_id} : {username}')
         result = event_bus.put_events(
             Entries=[{
                 'Source': 'auth-confirm-lambda',
                 'DetailType': 'UserRegistrationConfirmed',
                 'Detail': json.dumps({
-                    'user_id': user_id,
-                    'username': username
+                    "user_id": user_id,
+                    "username": username
                 }),
                 'EventBusName': os.environ.get('EVENT_BUS')
             }]
         )
 
-        logging.info(f'Event published: {result}')
-        logging.info(f'Returning response: {response}')
+        print(f'Event published: {result}')
+        print(f'Returning response: {response}')
 
         return {
             'statusCode': 200,
@@ -57,20 +56,20 @@ def lambda_handler(event, context):
         }
 
     except botocore.exceptions.ClientError as error:
-        logging.error(f'Error: {error}')
+        print(f'Error: {error}')
         if error.response['Error']['Code'] == 'CodeMismatchException':
-            logging.error(f'Code mismatch exception for user {username}')
+            print(f'Code mismatch exception for user {username}')
             return {
-                'statusCode': 400,
-                'body': json.dumps({
-                    'type': error.response['Error']['Code'],
-                    'message': error.response['Error']['Message']
+                "statusCode": 400,
+                "body": json.dumps({
+                    "type": error.response['Error']['Code'],
+                    "message": error.response['Error']['Message']
                 })
             }
 
 
 def retrieve_user_id(username):
-    logging.info(f'Retrieving user id for username {username}')
+    print(f'Retrieving user id for username {username}')
     response = boto3.client('cognito-idp').admin_get_user(
         UserPoolId=get_user_pool_id(),
         Username=username
@@ -80,17 +79,18 @@ def retrieve_user_id(username):
 
 def get_user_pool_id():
     stage_name = os.environ.get('StageName')
-    user_pool_id_parameter_name = "DelaygramAuthorizer-" + stage_name
+    user_pool_id_parameter_name = "DelaygramAuthorizerPoolId-" + stage_name
     return boto3.client('ssm').get_parameter(Name=user_pool_id_parameter_name)['Parameter']['Value']
 
 
 def get_client_id():
-    client_id_parameter_name = "DelaygramAuthorizerClient"
+    stage_name = os.environ.get('StageName')
+    client_id_parameter_name = "DelaygramAuthorizerClientId-" + stage_name
     return boto3.client('ssm').get_parameter(Name=client_id_parameter_name)['Parameter']['Value']
 
 
 def get_client_secret(user_pool_id, client_id):
-    response = boto3.client('cognito-idp').describe_user_pool(
+    response = boto3.client('cognito-idp').describe_user_pool_client(
         UserPoolId=user_pool_id,
         ClientId=client_id
     )
@@ -99,6 +99,6 @@ def get_client_secret(user_pool_id, client_id):
 
 def get_secret_hash(username, client_id, user_pool_id):
     client_secret = get_client_secret(user_pool_id, client_id)
-    message = bytes(username + client_id + 'utf-8')
+    message = bytes(username + client_id, 'utf-8')
     key = bytes(client_secret, 'utf-8')
     return base64.b64encode(hmac.new(key, message, digestmod=hashlib.sha256).digest()).decode()
--- functions/login/app.py
+++ functions/login/app.py
@@ -1,7 +1,6 @@
 import base64
 import hashlib
 import hmac
-import logging
 import os
 
 import boto3
@@ -13,14 +12,14 @@ def lambda_handler(event, context):
     cognito = boto3.client('cognito-idp')
     event_bus = boto3.client('events')
 
-    logging.info(f'Received event: {event}')
+    print(f'Received event: {event}')
 
     body = json.loads(event['body'])
     username = body['username']
     password = body['password']
 
     try:
-        logging.info(f'Attempting to authenticate user {username}')
+        print(f'Attempting to authenticate user {username}')
         response = cognito.initiate_auth(
             AuthFlow='USER_PASSWORD_AUTH',
             AuthParameters={
@@ -31,11 +30,11 @@ def lambda_handler(event, context):
             ClientId=get_client_id()
         )
 
-        logging.info(f'Authentication successful for user {username}')
+        print(f'Authentication successful for user {username}')
 
         user_id = retrieve_user_id(username)
 
-        logging.info(f'Publishing event for user {user_id} : {username}')
+        print(f'Publishing event for user {user_id} : {username}')
         result = event_bus.put_events(
             Entries=[{
                 'Source': 'auth-login-lambda',
@@ -48,8 +47,8 @@ def lambda_handler(event, context):
             }]
         )
 
-        logging.info(f'Event published: {result}')
-        logging.info(f'Returning response: {response}')
+        print(f'Event published: {result}')
+        print(f'Returning response: {response}')
         return {
             'statusCode': 200,
             'body': json.dumps({
@@ -58,10 +57,10 @@ def lambda_handler(event, context):
             })
         }
     except botocore.exceptions.ClientError as error:
-        logging.error(f'Client error: {error}')
+        print(f'Client error: {error}')
 
         if error.response['Error']['Code'] == 'UserNotConfirmedException':
-            logging.error(f'User {username} not confirmed')
+            print(f'User {username} not confirmed')
             return {
                 'statusCode': 403,
                 'body': json.dumps({
@@ -70,7 +69,7 @@ def lambda_handler(event, context):
                 })
             }
         elif error.response['Error']['Code'] == 'NotAuthorizedException':
-            logging.error(f'User {username} not authorized')
+            print(f'User {username} not authorized')
             return {
                 'statusCode': 404,
                 'body': json.dumps({
@@ -79,7 +78,7 @@ def lambda_handler(event, context):
                 })
             }
         elif error.response['Error']['Code'] == 'UserNotFoundException':
-            logging.error(f'User {username} not found')
+            print(f'User {username} not found')
             return {
                 'statusCode': 404,
                 'body': json.dumps({
@@ -90,7 +89,7 @@ def lambda_handler(event, context):
 
 
 def retrieve_user_id(username):
-    logging.info(f'Retrieving user id for username {username}')
+    print(f'Retrieving user id for username {username}')
     response = boto3.client('cognito-idp').admin_get_user(
         UserPoolId=get_user_pool_id(),
         Username=username
@@ -100,17 +99,18 @@ def retrieve_user_id(username):
 
 def get_user_pool_id():
     stage_name = os.environ.get('StageName')
-    user_pool_id_parameter_name = "DelaygramAuthorizer-" + stage_name
+    user_pool_id_parameter_name = "DelaygramAuthorizerPoolId-" + stage_name
     return boto3.client('ssm').get_parameter(Name=user_pool_id_parameter_name)['Parameter']['Value']
 
 
 def get_client_id():
-    client_id_parameter_name = "DelaygramAuthorizerClient"
+    stage_name = os.environ.get('StageName')
+    client_id_parameter_name = "DelaygramAuthorizerClientId-" + stage_name
     return boto3.client('ssm').get_parameter(Name=client_id_parameter_name)['Parameter']['Value']
 
 
 def get_client_secret(user_pool_id, client_id):
-    response = boto3.client('cognito-idp').describe_user_pool(
+    response = boto3.client('cognito-idp').describe_user_pool_client(
         UserPoolId=user_pool_id,
         ClientId=client_id
     )
@@ -119,6 +119,6 @@ def get_client_secret(user_pool_id, client_id):
 
 def get_secret_hash(username, client_id, user_pool_id):
     client_secret = get_client_secret(user_pool_id, client_id)
-    message = bytes(username + client_id + 'utf-8')
+    message = bytes(username + client_id, 'utf-8')
     key = bytes(client_secret, 'utf-8')
     return base64.b64encode(hmac.new(key, message, digestmod=hashlib.sha256).digest()).decode()
